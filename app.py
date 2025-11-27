cd844c56d85a844967f3dbd9f35fd3e3c5513b07
Daniel <daniel.symonds@education.nsw.gov.au>
2025-11-27 14:08:13 +1100

    Second Commit

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template, request, redirect
+import sqlite3
+import os 
+
+app = Flask(__name__)
+app.secret_key = os.urandom(24)
+
+@app.route('/')
+def login():
+    return render_template('login.html')
+
+@app.route('/login_validation', methods=['POST'])
+def login_validation():
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    connection = sqlite3.connect('loginData.db')
+    cursor = connection.cursor()
+
+    user = cursor.execute('SELECT * FROM USERS WHERE email=? AND password=?', (email, password)).fetchall()
+    if len(user) > 0:
+        return 'welcome'
+    else:
+        return redirect('/')
+
+if __name__ == '__main__':
+    app.run(debug=True)
